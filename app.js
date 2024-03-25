90293cd5a2166c93def803feae0c2c7d8424f0c6
ZakiyyullohDev <141619922+ZakiyyullohDev@users.noreply.github.com>
2024-03-25 15:24:22 +0500

    Finish this todo!

--- app.js
+++ app.js
@@ -1,154 +1,160 @@
-const todoInput = document.getElementById('todoInput');
+import { uuidv4 } from "./js/uuid.js";
+
+const todoList = JSON.parse(localStorage.getItem('todos')) || [];
+let allDeleteButtons = document.querySelectorAll('.delete-todo');
+let allEditButtons = document.querySelectorAll('.edit-todo');
 const todoAddBtn = document.getElementById('todoAddBtn');
+const todoInput = document.getElementById('todoInput');
 const todoUl = document.getElementById('todoUl');
-const editTodo = document.getElementById('editTodo')
-const deleteTodo = document.getElementById('deleteTodo')
-const todos = JSON.parse(localStorage.getItem('todos')) || []
-const todoList = todos.slice()
 
-const getItem = () => {
-
-    todos.forEach(todoText => {
-        const todoLi = document.createElement('li');
-        todoLi.textContent = todoText
-        todoLi.classList.add('todo-li');
-        const editBtn = document.createElement('button')
-        editBtn.classList.add('edit-todo')
-        editBtn.textContent = 'Edit'
-        editBtn.addEventListener('click', ()=> {
-            const editTodoPrompt = prompt('Todo Kiriritng:')
-            if (editTodoPrompt.length > 3) {
-                todoLi.textContent = editTodoPrompt
-                const index = todoList.indexOf(todoText);
+const setItemStorage = (id, todoText) => {
+    if (id && todoText) {
+        todoList.push(
+            {
+                id,
+                todoText:todoText
+            })
+        }
+        localStorage.setItem('todos', JSON.stringify(todoList));
+    }
+    
+    const getItemStorage = () => {
+        todoList.forEach(todo => {
+            
+            const todoLi = document.createElement('li');
+            todoLi.classList.add('todo-li')
+            todoLi.dataset.id = todo.id
+            
+            const todoP = document.createElement('p');
+            todoP.textContent = todo.todoText;
+            todoP.dataset.id = todo.id
+            
+            const editBtn = document.createElement('button');
+            editBtn.classList.add('edit-todo');
+            editBtn.textContent = 'Edit';
+            editBtn.dataset.id = todo.id
+            
+            const deleteBtn = document.createElement('button');
+            deleteBtn.classList.add('delete-todo');
+            deleteBtn.textContent = 'Delete';
+            deleteBtn.dataset.id = todo.id
+            
+            const todoBtnWrapper = document.createElement('div');
+            todoBtnWrapper.appendChild(editBtn);0
+            todoBtnWrapper.appendChild(deleteBtn);
+            
+            todoLi.appendChild(todoP);
+            todoLi.appendChild(todoBtnWrapper);
+            todoUl.appendChild(todoLi);
+        });
+        deleteTodoFunc()
+        editTodoFunc()
+    }
+    
+    const deleteTodoFunc = () => {
+        allDeleteButtons = document.querySelectorAll('.delete-todo')
+        
+        for (const deleteBtn of allDeleteButtons) {
+            deleteBtn.onclick = (e) => {
+                todoUl.removeChild(e.target.parentElement.parentElement);
+                const searchedTodo = todoList.find(todo => todo.id == e.target.dataset.id)
+                const index = todoList.indexOf(searchedTodo)
+                
                 if (index !== -1) {
-                    todoList[index] = editTodoPrompt;
+                    todoList.splice(index, 1);
                     setItemStorage();
                 }
-                
-                todoBtnWrapper.appendChild(deleteBtn)
-                todoLi.appendChild(todoBtnWrapper)
-                todoLi.appendChild(todoBtnWrapper)
-                todoList.push(todoInput.value)
-            } else {
-                const writeTodo = confirm('Iltimos Todo Kiriting!')
-                if (writeTodo) {
-                    prompt('Todo Kiritng:')
-                } else {
-                    confirm('Todo Kirit Iplos!')
-                    prompt('Todo Kirit marraz!:')
-                }
             }
-        })
-        
-        const deleteBtn = document.createElement('button')
-        deleteBtn.classList.add('delete-todo')
-        
-        deleteBtn.textContent = 'Delete'
-        deleteBtn.addEventListener('click', ()=> {
-            todoUl.removeChild(todoLi)
-            const index = todoList.indexOf(todoText);
-            if (index !== -1) {
-                todoList.splice(index, 1);
-                setItemStorage();
-            }
-        })
-        
-        const todoBtnWrapper = document.createElement('div')
-        
-        todoBtnWrapper.appendChild(editBtn)
-        todoBtnWrapper.appendChild(deleteBtn)
-        
-        todoLi.appendChild(todoBtnWrapper)
-        todoUl.appendChild(todoLi);
-    });
-}
-
-getItem()
-
-const setItemStorage = () => {
-    localStorage.setItem('todos', JSON.stringify(todoList))
-}
-
-const addTodo = ()=> {
-    const todoText = todoInput.value.trim();
+        }
+    }
     
-    if (!todoText) {
-        alert('Iltimos todo kiriting');
-        return;
-
-    } else if (todoText.length > 40) {
-        alert("Iltimos 40 ta so'zdan kamroq yozing!")
-
-        todoInput.value = ''
-
-    } else {
+    const editTodoFunc = () => {
+        allEditButtons = document.querySelectorAll('.edit-todo');
+        let todoTextP = document.querySelectorAll('p')
         
-        todoUl.classList.remove('display-none')
+        for (const editBtn of allEditButtons) {
+            editBtn.addEventListener('click', (e) => {
+                const text = e.target.parentElement.parentElement.firstChild
+                text.setAttribute('contenteditable', true)
+                text.focus()
+
+                for (const todo of todoTextP) {
+                    text.addEventListener('keydown', (e) => {
+                        if (e.key == 'Enter') {
+                            text.setAttribute('contenteditable', false)
+                            const newValue = e.target.textContent
+                            const itemId = e.target.dataset.id
+                            const searchedTodo = todoList.find(todo => todo.id == e.target.dataset.id)
+                            const index = todoList.indexOf(searchedTodo)
+                            if (index !== -1) {
+                                todoList.splice(index, 1)
+                                setItemStorage(itemId, newValue)
+                            }        
+                        }
+                    })
+                }
+            })
+        }
+    }
+    
+    const addTodo = () => {
+        const todoText = todoInput.value.trim();
+        
+        if (!todoText) {
+            alert('Iltimos todo kiriting!')
+            return ''
+        }
+        
+        if (todoText.length > 50) {
+            alert("Iltimos 50 ta so'zdan kam yozing!")
+            todoInput.value = ''
+            return ''
+        }
+        
+        const id = uuidv4()
         
         const todoLi = document.createElement('li')
-        todoLi.textContent = todoInput.value
         todoLi.classList.add('todo-li')
+        todoLi.dataset.id = id
         
-        const editBtn = document.createElement('button')
-        editBtn.classList.add('edit-todo')
-        editBtn.textContent = 'Edit'
+        const todoP = document.createElement('p')
+        todoP.textContent = todoText
+        todoP.dataset.id = todoLi.id
         
-        editBtn.addEventListener('click', ()=> {
-            const editTodoPrompt = prompt('Todo Kiriritng:')
-            if (editTodoPrompt.length > 3) {
-                todoLi.textContent = editTodoPrompt
-                const index = todoList.indexOf(todoText);
-                if (index !== -1) {
-                    todoList[index] = editTodoPrompt;
-                    setItemStorage();
-                }
-                
-                todoBtnWrapper.appendChild(deleteBtn)
-                todoLi.appendChild(todoBtnWrapper)
-                todoList.push(editTodoPrompt)
-
-            } else {
-                const writeTodo = confirm('Iltimos Todo Kiriting!')
-                if (writeTodo) {
-                    prompt('Todo Kiriritng:')
-                } else {
-                    confirm('Todo Kirit Iplos!')
-                }
-            }
-        })
+        const editTodo = document.createElement('button')
+        editTodo.textContent = 'Edit'
+        editTodo.classList.add('edit-todo')
+        editTodo.dataset.id = todoLi.id
         
-        const deleteBtn = document.createElement('button')
-        deleteBtn.classList.add('delete-todo')
-        deleteBtn.textContent = 'Delete'
+        const deleteTodo = document.createElement('button')
+        deleteTodo.textContent = 'Delete'
+        deleteTodo.classList.add('delete-todo')
+        deleteTodo.dataset.id = todoLi.id
         
-        const todoBtnWrapper = document.createElement('div')
+        const todoBtnsWrapper = document.createElement('div')
+        todoBtnsWrapper.appendChild(editTodo)
+        todoBtnsWrapper.appendChild(deleteTodo)
         
-        todoBtnWrapper.appendChild(editBtn)
-        todoBtnWrapper.appendChild(deleteBtn)
-        deleteBtn.addEventListener('click', ()=> {
-            todoUl.removeChild(todoLi)
-            const index = todoList.indexOf(todoText);
-            if (index !== -1) {
-                todoList.splice(index, 1);
-                setItemStorage();
-            }
-        })
-        
-        todoLi.appendChild(todoBtnWrapper)
+        todoLi.appendChild(todoP)
+        todoLi.appendChild(todoBtnsWrapper)
         
         todoUl.appendChild(todoLi)
-        todoList.push(todoInput.value)
-        setItemStorage()
+        
         todoInput.value = ''
+        setItemStorage(id, todoText)
+        
+        deleteTodoFunc()
+        editTodoFunc()
     }
-}
-
-todoAddBtn.addEventListener('click', ()=> {
-    addTodo()
-})
-
-todoInput.addEventListener('keydown', (e)=> {
-    if (e.key === "Enter") {
-        addTodo()      
-    }
-})
+    
+    todoAddBtn.addEventListener('click', ()=> {
+        addTodo()
+    })
+    
+    todoInput.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+            addTodo()
+        }
+    })
+    
+    getItemStorage()
